a97287ac7ee839b78d57cce9d74767fffbf15731
박유진 <qkrdbw@naver.com>
2023-06-28 18:49:45 +0900

    4. Challenge 3 of 3

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
+
+const baseUrl = 'https://i.imgur.com/';
 const person = {
   name: 'Gregorio Y. Zara',
-  image: "https://i.imgur.com/7vQD0fPs.jpg",
+  imageId: '7vQD0fP',
+  imageSize: 'b',
   theme: {
     backgroundColor: 'black',
     color: 'pink'
@@ -13,8 +16,8 @@ export default function TodoList() {
       <h1>{person.name}'s Todos</h1>
       <img
         className="avatar"
-        src={person.image}
-        alt="Gregorio Y. Zara"
+        src={baseUrl + person.imageId + person.imageSize + '.jpg'}
+        alt={person.name}
       />
       <ul>
         <li>Improve the videophone</li>
