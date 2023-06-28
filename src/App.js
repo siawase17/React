e25ef4c2760ef53ab6641a6aa94ab88f560cb15e
박유진 <qkrdbw@naver.com>
2023-06-28 19:28:32 +0900

    5. Challenge 1 of 3

--- src/App.js
+++ src/App.js
@@ -1,29 +1,59 @@
+import { getImageUrl } from './utils.js';
 
-const baseUrl = 'https://i.imgur.com/';
-const person = {
-  name: 'Gregorio Y. Zara',
-  imageId: '7vQD0fP',
-  imageSize: 'b',
-  theme: {
-    backgroundColor: 'black',
-    color: 'pink'
-  }
-};
-
-export default function TodoList() {
+function Profile({
+  imageId, name, profession, awards, discovery, imageSize = 70
+}) {
   return (
-    <div style={person.theme}>
-      <h1>{person.name}'s Todos</h1>
+    <section className="profile">
+      <h2>{name}</h2>
       <img
         className="avatar"
-        src={baseUrl + person.imageId + person.imageSize + '.jpg'}
-        alt={person.name}
+        src={getImageUrl(imageId)}
+        alt={name}
+        width={imageSize}
+        height={imageSize}
       />
       <ul>
-        <li>Improve the videophone</li>
-        <li>Prepare aeronautics lectures</li>
-        <li>Work on the alcohol-fuelled engine</li>
+        <li><b>Profession:</b> {profession}</li>
+        <li>
+          <b>Awards: {awards.length} </b>
+          ({awards.join(', ')})
+        </li>
+        <li>
+          <b>Discovered: </b>
+          {discovery}
+        </li>
       </ul>
+    </section>
+  );
+}
+
+export default function Gallery() {
+  return (
+    <div>
+      <h1>Notable Scientists</h1>
+      <Profile
+        imageId="szV5sdG"
+        name="Maria Skłodowska-Curie"
+        profession="physicist and chemist"
+        awards={[
+        'Nobel Prize in Physics',
+        'Nobel Prize in Chemistry',
+        'Davy Medal',
+        'Matteucci Medal'
+        ]}      
+        discovery="polonium (chemical element)"
+      />
+      <Profile
+        imageId='YfeOqp2'
+        name='Katsuko Saruhashi'
+        profession='geochemist'
+        awards={[
+          'Miyake Prize for geochemistry',
+          'Tanaka Prize'
+        ]}
+        discovery="a method for measuring carbon dioxide in seawater"
+      />
     </div>
   );
 }
