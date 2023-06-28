1e833781a655d7b434b21bde23708a990acd6744
박유진 <qkrdbw@naver.com>
2023-06-29 02:36:41 +0900

    7-3. 리스트 항목 컴포넌트 추출

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
 import { recipes } from './data.js';
 
+function Recipe({id, name, ingredients}) {
+  return (
+    <div>
+      <h2>{name}</h2>
+      <ul>
+        {ingredients.map(ingredient =>
+          <li key={ingredient}>
+            {ingredient}
+          </li>
+        )}
+      </ul>
+    </div>
+  )
+}
+
 export default function RecipeList() {
   return (
     <div>
       <h1>Recipes</h1>
       {recipes.map(recipe =>
-        <div key={recipe.id}>
-          <h2>{recipe.name}</h2>
-          <ul>
-            {recipe.ingredients.map(ingredient =>
-              <li key={ingredient}>
-                {ingredient}
-              </li>
-            )}
-          </ul>
-        </div>
+        <Recipe {...recipe} key={recipe.id}/>
       )}
     </div>
   );
