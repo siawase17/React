dce8ce075c9f21658eda6d3a9582d09be3b9c6c3
박유진 <qkrdbw@naver.com>
2023-07-01 19:47:57 +0900

    1-1. 이벤트 핸들러 수정

--- src/App.js
+++ src/App.js
@@ -9,4 +9,20 @@ export default function StoryTray({ stories }) {
       <li>Create Story</li>
     </ul>
   );
+}export default function LightSwitch() {
+  function handleClick() {
+    let bodyStyle = document.body.style;
+    if (bodyStyle.backgroundColor === 'black') {
+      bodyStyle.backgroundColor = 'white';
+    } else {
+      bodyStyle.backgroundColor = 'black';
+    }
+  }
+
+  return (
+    <button onClick={handleClick}>
+      Toggle the lights
+    </button>
+  );
 }
+
