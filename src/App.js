16b7cca3d0e529bea5fb2bd84528f18218cedfd7
박유진 <qkrdbw@naver.com>
2023-07-01 20:07:09 +0900

    1-2. 이벤트 상위 전파 중지

--- src/App.js
+++ src/App.js
@@ -1,28 +1,31 @@
-export default function StoryTray({ stories }) {
-  return (
-    <ul>
-      {stories.map(story => (
-        <li key={story.id}>
-          {story.label}
-        </li>
-      ))}
-      <li>Create Story</li>
-    </ul>
-  );
-}export default function LightSwitch() {
-  function handleClick() {
+import { useState } from 'react';
+import ColorSwitch from './ColorSwitch.js';
+
+export default function App() {
+  const [clicks, setClicks] = useState(0);
+
+  function handleClickOutside() {
+    setClicks(c => c + 1);
+  }
+
+  function getRandomLightColor() {
+    let r = 150 + Math.round(100 * Math.random());
+    let g = 150 + Math.round(100 * Math.random());
+    let b = 150 + Math.round(100 * Math.random());
+    return `rgb(${r}, ${g}, ${b})`;
+  }
+
+  function handleChangeColor() {
     let bodyStyle = document.body.style;
-    if (bodyStyle.backgroundColor === 'black') {
-      bodyStyle.backgroundColor = 'white';
-    } else {
-      bodyStyle.backgroundColor = 'black';
-    }
+    bodyStyle.backgroundColor = getRandomLightColor();
   }
 
   return (
-    <button onClick={handleClick}>
-      Toggle the lights
-    </button>
+    <div style={{ width: '100%', height: '100%' }} onClick={handleClickOutside}>
+      <ColorSwitch onChangeColor={handleChangeColor} />
+      <br />
+      <br />
+      <h2>Clicks on the page: {clicks}</h2>
+    </div>
   );
 }
-
