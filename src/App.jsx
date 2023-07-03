63dc115179c1f4f108a9b7083adb64d0ca47684c
박유진 <qkrdbw@naver.com>
2023-07-03 12:20:48 +0900

    2-3. 훅(useState) 컴포넌트의 최상위레벨에서만 호출

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,26 @@
-import { useState } from "react";
+import { useState } from 'react';
 
-export default function Form() {
-  const [firstName, setFirstName] = useState('');
-  const [lastName, setLastName] = useState('');
+export default function FeedbackForm() {
+  const [isSent, setIsSent] = useState(false);
+  const [message, setMessage] = useState('');
 
-  function handleFirstNameChange(e) {
-    setFirstName(e.target.value);
-  }
-
-  function handleLastNameChange(e) {
-    setLastName(e.target.value);
-  }
-
-  function handleReset() {
-    setFirstName('');
-    setLastName('');
+  if (isSent) {
+    return <h1>Thank you!</h1>;
   }
 
   return (
-    <form onSubmit={e => e.preventDefault()}>
-      <input
-        placeholder="First name"
-        value={firstName}
-        onChange={handleFirstNameChange}
+    <form onSubmit={e => {
+      e.preventDefault();
+      alert(`Sending: "${message}"`);
+      setIsSent(true);
+    }}>
+      <textarea
+        placeholder="Message"
+        value={message}
+        onChange={e => setMessage(e.target.value)}
       />
-      <input
-        placeholder="Last name"
-        value={lastName}
-        onChange={handleLastNameChange}
-      />
-      <h1>Hi, {firstName}{lastName}</h1>
-      <button onClick={handleReset}>Reset</button>
+      <br />
+      <button type="submit">Send</button>
     </form>
   );
 }
