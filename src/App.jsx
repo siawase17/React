8bcee1bf48d6eb6815688a263ac3de197c8781a3
박유진 <qkrdbw@naver.com>
2023-07-03 14:04:03 +0900

    4. 신호등 구현하기

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
 import { useState } from 'react';
 
-export default function FeedbackForm() {
+export default function TrafficLight() {
+  const [walk, setWalk] = useState(true);
+
   function handleClick() {
-    const name = prompt('What is your name?');
-    alert(`Hello, ${name}!`);
-  }
+    setWalk(!walk);
+    alert(walk ? '다음은 정지입니다.' : '다음은 걷기입니다.');
+}
 
   return (
-    <button onClick={handleClick}>
-      Greet
-    </button>
+    <>
+      <button onClick={handleClick}>
+        Change to {walk ? 'Stop' : 'Walk'}
+      </button>
+      <h1 style={{
+        color: walk ? 'darkgreen' : 'darkred'
+      }}>
+        {walk ? 'Walk' : 'Stop'}
+      </h1>
+    </>
   );
 }
