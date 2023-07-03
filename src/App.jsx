c10c3f22af5327af50729afe517e598fbefc1554
박유진 <qkrdbw@naver.com>
2023-07-03 11:58:54 +0900

    2-2. useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,36 @@
-import { useState } from 'react';
-import { sculptureList } from './data.js';
+import { useState } from "react";
 
-export default function Gallery() {
-  const [index, setIndex] = useState(0);
-  const [showMore, setShowMore] = useState(false);
+export default function Form() {
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
 
-  let prev = index > 0;
-  let next = index < sculptureList.length - 1;
-
-  function handleNextClick() {
-    if (next) {
-      setIndex(index + 1);
-    }
+  function handleFirstNameChange(e) {
+    setFirstName(e.target.value);
   }
 
-  function handlePrevClick() {
-    if (prev) {
-      setIndex(index - 1);
-    }
+  function handleLastNameChange(e) {
+    setLastName(e.target.value);
   }
 
-  function handleMoreClick() {
-    setShowMore(!showMore);
+  function handleReset() {
+    setFirstName('');
+    setLastName('');
   }
 
-  let sculpture = sculptureList[index];
   return (
-    <>
-      <button onClick={handleNextClick} disabled={!next}>
-        {/* next가 False인 경우, Next 버튼 비활성화 */}
-        Next
-      </button>
-      <button onClick={handlePrevClick} disabled={!prev}>
-        {/* prev가 False인 경우, Previous 버튼 비활성화 */}
-        Previous
-      </button>  
-      <h2>
-        <i>{sculpture.name} </i>
-        by {sculpture.artist}
-      </h2>
-      <h3>
-        ({index + 1} of {sculptureList.length})
-      </h3>
-      <button onClick={handleMoreClick}>
-        {showMore ? 'Hide' : 'Show'} details
-      </button>
-      {showMore && <p>{sculpture.description}</p>}
-      <img
-        src={sculpture.url}
-        alt={sculpture.alt}
+    <form onSubmit={e => e.preventDefault()}>
+      <input
+        placeholder="First name"
+        value={firstName}
+        onChange={handleFirstNameChange}
       />
-    </>
+      <input
+        placeholder="Last name"
+        value={lastName}
+        onChange={handleLastNameChange}
+      />
+      <h1>Hi, {firstName}{lastName}</h1>
+      <button onClick={handleReset}>Reset</button>
+    </form>
   );
 }
